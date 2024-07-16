71c90080ad3895133959028465afeb6bfdcb23f1
Benjamin-chidera <benjaminchidera72@gmail.com>
2024-07-16 23:15:30 +0100

    refactor: changed env

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/jobs", "/employer/post-jobs", "/get-jobs", "/jobs/:path*"],
+  matcher: [ "/employer/post-jobs", "/get-jobs", "/jobs/:path*"],
 };
